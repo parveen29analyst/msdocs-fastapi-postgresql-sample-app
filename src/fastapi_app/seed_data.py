788acf7e18c7527f38bd859760a104beac273c5e
parveen29analyst <71768940+parveen29analyst@users.noreply.github.com>
2025-04-15 19:37:14 +0530

    Update seed_data.py

--- src/fastapi_app/seed_data.py
+++ src/fastapi_app/seed_data.py
@@ -1,14 +1,15 @@
 from sqlmodel import SQLModel
 
-from fastapi_app.models import Restaurant, Review, create_db_and_tables, engine
+# from fastapi_app.models import Restaurant, Review, create_db_and_tables, engine
 
 
-def drop_all():
-    # Explicitly remove these tables first to avoid cascade errors
-    SQLModel.metadata.remove(Restaurant.__table__)
-    SQLModel.metadata.remove(Review.__table__)
-    SQLModel.metadata.drop_all(engine)
+# def drop_all():
+#     # Explicitly remove these tables first to avoid cascade errors
+#     SQLModel.metadata.remove(Restaurant.__table__)
+#     SQLModel.metadata.remove(Review.__table__)
+#     SQLModel.metadata.drop_all(engine)
 
 
 if __name__ == "__main__":
-    create_db_and_tables()
+    print('working on fastapi')
+    # create_db_and_tables()
